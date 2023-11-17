34dd94bcf08789fdd4110afc60b4751a66cbdea9
AnggaGemilang <anggagemilang12@gmail.com>
2023-11-17 08:26:07 +0700

    FE done

--- fertigation-app/app/src/main/java/com/agrapana/fertigation/ui/activity/WorkerActivity.kt
+++ fertigation-app/app/src/main/java/com/agrapana/fertigation/ui/activity/WorkerActivity.kt
@@ -2,11 +2,69 @@ package com.agrapana.fertigation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
 import com.agrapana.fertigation.R
+import com.agrapana.fertigation.databinding.ActivityMainBinding
+import com.agrapana.fertigation.databinding.ActivityWorkerBinding
+import com.agrapana.fertigation.ui.fragment.AddFieldFragment
+import com.agrapana.fertigation.ui.fragment.AddWorkerFragment
+import com.agrapana.fertigation.ui.fragment.CropRecommendationFragment
 
 class WorkerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWorkerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_worker)
+        binding = ActivityWorkerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        val w = window
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        setSupportActionBar(binding.toolbar);
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = "Workers"
+
+        binding.fab.setOnClickListener {
+            val dialog = AddWorkerFragment()
+            val bundle = Bundle()
+            bundle.putString("status", "tambah")
+            dialog.arguments = bundle
+            dialog.show(supportFragmentManager, "BottomSheetDialog")
+        }
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.action_nav4, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.about -> {
+                AlertDialog.Builder(this)
+                    .setTitle("App Version")
+                    .setMessage("Beta 1.0.0")
+                    .setCancelable(true)
+                    .setPositiveButton("OK", null)
+                    .create()
+                    .show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
